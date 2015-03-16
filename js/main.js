329de8ec2b5d336f773ad824222b2b0893696d98
Aram Baghdasaryan <xquack@gmail.com>
2015-03-17 00:48:24 +0400

    MVP complete

--- js/main.js
+++ js/main.js
@@ -5,16 +5,52 @@ var getLocation = function(href) {
 };
 
 $(function() {
-//	chrome.tabs.getSelected(null, function(tab) {
-//		var tablink = tab.url,
-//			url = getLocation(tablink);
-//
-//		if (url.hostname == 'app.asana.com') {
-			$.get("https://app.asana.com/api/1.0/projects/18203401877896/tasks", function(data) {
-				for (var i in data.data) {
-					$('.tasks').append('<li>' + data.data[i].name + '</li>');
-				}
-			}, "json");
-//		}
-//	});
+	var $total_hours = $('.total-hours'),
+		$total_stories = $('.total-stories'),
+		$not_assigned = $('.not-assigned');
+
+	chrome.tabs.getSelected(null, function(tab) {
+		var tablink = tab.url,
+			url = getLocation(tablink);
+
+		$total_hours.text(0);
+		$total_stories.text(0);
+		$not_assigned.text(0);
+
+		if (url.hostname == 'app.asana.com') {
+			var pathname = url.pathname,
+				parts = pathname.split('/'),
+				needle = null,
+				total_hours = 0,
+				total_stories = 0,
+				total_not_assigned_stories = 0;
+
+			if (parts.length > 2) {
+				$.get('https://app.asana.com/api/1.0/projects/' + parts[2] + '/tasks', function(data) {
+					for (var i in data.data) {
+						if (data.data.hasOwnProperty(i)) {
+							needle = /\[([\d\.]+)\]/.exec(data.data[i].name);
+
+							if (needle !== null) {
+								needle = parseFloat(needle[1]);
+
+								total_hours += needle;
+								total_stories++;
+
+								console.log('total_hours', total_hours);
+								console.log('total_stories', total_stories);
+							} else {
+								total_not_assigned_stories++;
+								console.log('total_not_assigned_stories', total_not_assigned_stories);
+							}
+						}
+					}
+
+					$total_hours.text(total_hours);
+					$total_stories.text(total_stories);
+					$not_assigned.text(total_not_assigned_stories);
+				}, "json");
+			}
+		}
+	});
 });
