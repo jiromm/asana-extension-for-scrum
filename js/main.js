866ca3fe5a73b0768ff8b40d0a3221cd44726947
Aram Baghdasaryan <xquack@gmail.com>
2015-04-18 00:17:59 +0400

    Preparations for applied factor implementation

--- js/main.js
+++ js/main.js
@@ -1,88 +1,72 @@
 var getLocation = function(href) {
-	var l = document.createElement("a");
-	l.href = href;
+		var l = document.createElement("a");
+		l.href = href;
 
-	return l;
-};
-var users = {
-	10124585031848: {
-		"name": "Eduard Aleksanyan",
-		"image": "https://s3.amazonaws.com/profile_photos/10124585031848.wyUMUWeGEyfd7bXATcGa_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
+		return l;
 	},
-	11686527531417: {
-		"name": "Tigran Ghabuzyan",
-		"image": "https://s3.amazonaws.com/profile_photos/11686527531417.QOvoSX7R6lTJSG9VDBQI_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
+	users = {
+		10124585031848: {
+			"name": "Eduard Aleksanyan",
+			"image": "https://s3.amazonaws.com/profile_photos/10124585031848.wyUMUWeGEyfd7bXATcGa_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
+		},
+		11686527531417: {
+			"name": "Tigran Ghabuzyan",
+			"image": "https://s3.amazonaws.com/profile_photos/11686527531417.QOvoSX7R6lTJSG9VDBQI_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
+		},
+		10124585031839: {
+			"name": "Tigran Petrosyan",
+			"image": "https://s3.amazonaws.com/profile_photos/10124585031839.Zvk5gKoGkI4BZNXFeAG7_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
+		},
+		5754650264628: {
+			"name": "Tigran Tadevosyan",
+			"image": "https://s3.amazonaws.com/profile_photos/5754650264628.J6jyaDiKxliFxx4kIpiU_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
+		},
+		25719136431202: {
+			"name": "Hrayr Papikyan",
+			"image": "https://s3.amazonaws.com/profile_photos/25719136431202.G39up5gCwyUO8Lg5UhBL_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
+		},
+		12950252745597: {
+			"name": "Arbi Baghoomian",
+			"image": "https://s3.amazonaws.com/profile_photos/12950252745597.xddYtxsUKBXFP4oYLpSF_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
+		},
+		10124585031830: {
+			"name": "Aram Baghdasaryan",
+			"image": "https://s3.amazonaws.com/profile_photos/10124585031830.JrvDk4UBi3WC0VX8vMT7_21x21.png",
+			"hours": {
+				"total": 0,
+				"completed": 0
+			}
 		}
-	},
-	10124585031839: {
-		"name": "Tigran Petrosyan",
-		"image": "https://s3.amazonaws.com/profile_photos/10124585031839.Zvk5gKoGkI4BZNXFeAG7_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	},
-	5754650264628: {
-		"name": "Tigran Tadevosyan",
-		"image": "https://s3.amazonaws.com/profile_photos/5754650264628.J6jyaDiKxliFxx4kIpiU_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	},
-	25719136431202: {
-		"name": "Hrayr Papikyan",
-		"image": "https://s3.amazonaws.com/profile_photos/25719136431202.G39up5gCwyUO8Lg5UhBL_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	},
-	12950252745597: {
-		"name": "Arbi Baghoomian",
-		"image": "https://s3.amazonaws.com/profile_photos/12950252745597.xddYtxsUKBXFP4oYLpSF_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	},
-	10124585031830: {
-		"name": "Aram Baghdasaryan",
-		"image": "https://s3.amazonaws.com/profile_photos/10124585031830.JrvDk4UBi3WC0VX8vMT7_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	}/*,
-	26433177772854: {
-		"name": "Astghik Hakobyan",
-		"image": "https://s3.amazonaws.com/profile_photos/26433177772854.4ep7rXLA9N8tLpuqeZZB_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	},
-	8430834800772: {
-		"name": "Amoor Avakian",
-		"image": "https://s3.amazonaws.com/profile_photos/8430834800772.1GM1v68dVWh5Sgem5vWm_21x21.png",
-		"hours": {
-			"total": 0,
-			"completed": 0
-		}
-	}*/
-};
+	};
 
 $(function() {
 	var $total_hours = $('.total-hours'),
 		$total_stories = $('.total-stories'),
-		$not_assigned = $('.not-assigned'),
+		$assigned = $('.assigned'),
 		$users = $('.users'),
 		$wrapper = $('.wrapper'),
 		$loader = $('.loader'),
@@ -93,17 +77,13 @@ $(function() {
 			var tablink = arrayOfTabs[0].url,
 				url = getLocation(tablink);
 
-			$total_hours.text(0);
-			$total_stories.text(0);
-			$not_assigned.text(0);
-
 			if (url.hostname == 'app.asana.com') {
 				var pathname = url.pathname,
 					parts = pathname.split('/'),
 					needle = null,
-					total_hours = 0,
-					total_stories = 0,
-					total_not_assigned_stories = 0,
+					totalHours = 0,
+					totalStories = 0,
+					totalAssignedStories = 0,
 					hour,
 					task,
 					taskName,
@@ -130,8 +110,8 @@ $(function() {
 								if (needle !== null) {
 									hour = parseFloat(needle[1]);
 
-									total_hours += hour;
-									total_stories++;
+									totalHours += hour;
+									totalStories++;
 
 									if (taskAssignee !== null) {
 										if (users.hasOwnProperty(taskAssignee.id)) {
@@ -140,17 +120,17 @@ $(function() {
 											if (taskCompleted) {
 												users[taskAssignee.id]['hours']['completed'] += hour;
 											}
+
+											totalAssignedStories++;
 										}
 									}
-								} else {
-									total_not_assigned_stories++;
 								}
 							}
 						}
 
-						$total_hours.text(total_hours);
-						$total_stories.text(total_stories);
-						$not_assigned.text(total_not_assigned_stories);
+						$total_hours.text(totalHours);
+						$total_stories.text(totalStories);
+						$assigned.text(totalAssignedStories);
 
 						for (var j in users) {
 							if (users.hasOwnProperty(j)) {
@@ -159,7 +139,7 @@ $(function() {
 										'<img src="' + users[j]['image'] + '" width="21" height="21"> &nbsp; ' +
 										'<span class="badge">' + users[j]['hours']['completed'] + ' / ' + users[j]['hours']['total'] + '</span>' +
 										users[j]['name'] +
-									'</a>'
+										'</a>'
 								);
 							}
 						}
